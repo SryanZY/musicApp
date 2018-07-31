ebae903c5353c8a10eab9778bd77a5161e94f1ac
zhaoye <zhaoye@fltrp.com>
2018-07-31 16:50:57 +0800

    project init

--- music/src/main.js
+++ music/src/main.js
@@ -2,14 +2,16 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import FastClick from 'fastclick'
+import 'common/stylus/index.styl'
 
 Vue.config.productionTip = false
+
+FastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
-    router,
     components: { App },
     template: '<App/>'
 })
