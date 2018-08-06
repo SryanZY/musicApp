c5ab56723b498241a97e0d2d89c66425f9fe85d0
zhaoye <zhaoye@fltrp.com>
2018-08-06 16:52:23 +0800

    新版本的vue-cli调用远程设置了host和referer的数据&添加1像素边框

--- music/src/components/recommend/recommend.vue
+++ music/src/components/recommend/recommend.vue
@@ -6,6 +6,17 @@
             </div>
             <div class="recommend-list">
                 <h1 class="list-title">热门歌单推荐</h1>
+                <ul>
+                    <li v-for="(item, index) in discList" class="item" :key="index">
+                        <div class="icon">
+                            <img :src="item.imgurl" width="60" height="60" alt="歌单">
+                        </div>
+                        <div class="text">
+                            <h2 class="name" v-html="item.creator.name"></h2>
+                            <p class="desc" v-html="item.dissname"></p>
+                        </div>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
@@ -13,23 +24,32 @@
 
 <script>
 import Swiper from 'base/swiper/swiper'
-import { getRecommend } from 'api/recommend'
+import { getRecommend, getDiscList } from 'api/recommend'
 import { ERR_OK } from 'api/config'
 export default {
     name: 'recommend',
     data () {
         return {
-            recommends: []
+            recommends: [],
+            discList: []
         }
     },
     created () {
         this._getRecommend()
+        this._getDiscList()
     },
     methods: {
         _getRecommend () {
             getRecommend().then((res) => {
                 if (res.code === ERR_OK) {
                     this.recommends = res.data.slider
+                }
+            })
+        },
+        _getDiscList () {
+            getDiscList().then((res) => {
+                if (res.code === ERR_OK) {
+                    this.discList = res.data.list
                 }
             })
         }
@@ -60,4 +80,26 @@ export default {
                     text-align: center
                     font-size: $font-size-medium
                     color: $color-theme
+                .item
+                    display: flex
+                    box-sizing: border-box
+                    align-items: center
+                    padding: 0 20px 20px 20px
+                    .icon
+                        flex 0 0 60px
+                        width 60px
+                        padding-right 20px
+                    .text
+                        flex: 1
+                        display: flex
+                        flex-direction: column
+                        justify-content: center
+                        line-height: 20px
+                        overflow: hidden
+                        font-size: $font-size-medium
+                        .name
+                            margin-bottom: 10px
+                            color: $color-text
+                        .desc
+                            color: $color-text-d
 </style>
