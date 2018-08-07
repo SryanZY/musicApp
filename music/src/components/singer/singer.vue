45c507dc629defb1a61dc96fcea30d942c4876e0
zhaoye <zhaoye@fltrp.com>
2018-08-07 16:05:14 +0800

    歌手页面数据拆分与排序，添加keep-alive

--- music/src/components/singer/singer.vue
+++ music/src/components/singer/singer.vue
@@ -1,16 +1,91 @@
 <template>
-    <div>singer</div>
+    <div class="singer">
+        <list-view :data="singers"></list-view>
+    </div>
 </template>
 
 <script>
+import { getSingerList } from 'api/singer'
+import { ERR_OK } from 'api/config'
+import Singer from 'common/js/singer'
+import ListView from 'base/listview/listview'
+
+const HOT_NAME = '热门'
+const HOT_SINGER_LEN = 10
+
 export default {
     data () {
-        return {}
+        return {
+            singers: []
+        }
     },
-    components: {}
+    components: {
+        ListView
+    },
+    created () {
+        this._getSingerList()
+    },
+    methods: {
+        _getSingerList () {
+            getSingerList().then(res => {
+                if (res.code === ERR_OK) {
+                    this.singers = this._normalizeSinger(res.data.list)
+                }
+            })
+        },
+        // 处理数据，使之按顺序排列
+        _normalizeSinger (list) {
+            let map = {
+                hot: {
+                    title: HOT_NAME,
+                    items: []
+                }
+            }
+            list.forEach((item, index) => {
+                if (index < HOT_SINGER_LEN) {
+                    map.hot.items.push(new Singer({
+                        id: item.Fsinger_mid,
+                        name: item.Fsinger_name
+                    }))
+                }
+                const key = item.Findex
+                if (!map[key]) {
+                    map[key] = {
+                        title: key,
+                        items: []
+                    }
+                }
+                map[key].items.push(new Singer({
+                    id: item.Fsinger_mid,
+                    name: item.Fsinger_name
+                }))
+            })
+            // 得到有序列表，处理map
+            let hot = []
+            let ret = []
+            for (let key in map) {
+                let val = map[key]
+                if (val.title.match(/[a-zA-Z]/)) {
+                    ret.push(val)
+                } else if (val.title === HOT_NAME) {
+                    hot.push(val)
+                }
+            }
+            // a-z排序,charCodeAt返回指定位置的字符的 Unicode 编码
+            ret.sort((a, b) => {
+                return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+            })
+            return hot.concat(ret)
+        }
+    }
 
 }
 
 </script>
-<style lang='stylus' module>
+<style lang='stylus' scoped>
+    .singer
+        position fixed
+        top 88px
+        bottom 0
+        width 100%
 </style>
